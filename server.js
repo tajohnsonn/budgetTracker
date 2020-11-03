988bea21579e2de8b484abbebccb7e9f6954952b
sunshine <tajohnsonn@gmail.com>
2020-11-03 14:57:03 -0800

    organized files and added comments to server.js file

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+// require packages here
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+// local host
 const PORT = 3000;
 
+// express inside of variable
 const app = express();
 
 app.use(logger("dev"));
 
+// setting up middleware
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// setting root to public
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
@@ -23,6 +28,7 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
+// shows what port app is assigned to
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
